478e6ebda40b5fd71d219e3e1e8a044cd7140a08
tmdgy6232 <tmdgy6232@naver.com>
2020-06-24 20:48:25 +0900

    2-2 data type 기본정수형 Int

---
+++ src/sec02/exam_03_int/IntExample.java
@@ -0,0 +1,25 @@
+package sec02.exam_03_int;
+
+public class IntExample {
+
+    public static void main(String[] args) {
+
+        /**
+         *  int 타입
+         *
+         * int 타입은 자바의 정수연산을 할때 defalult로 사용하는 타입이다.
+         * 아래의 byte나 short을 사용하여 정수연산을 하더라고 결국 int형으로 변환 후 작업하기 때문에
+         * 연산 속도에 있어선 차이가 나지 않는다.
+         *
+         */
+
+        int var1 = 10;
+        int var2 = 012;
+        int var3 = 0xA;
+
+        //int var4 = 1000000000000;
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+    }
+}
