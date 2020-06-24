165cf96ea9fdedb399c14c772f219ba92a4214b9
tmdgy6232 <tmdgy6232@naver.com>
2020-06-24 20:54:03 +0900

    2-2 long type

---
+++ src/sec02/exam_04_long/LongExample.java
@@ -0,0 +1,20 @@
+package sec02.exam_04_long;
+
+public class LongExample {
+    public static void main(String[] args) {
+        /**
+         * Long 타입
+         *
+         * 8바이트 메모리를 가지는 타입으로서
+         * 숫자 끝에 L을 붙혀주지 않으면 컴파일러가 4바이트로 인식하기 때문에 에러를 뱉는다.
+         * 숫자 끝에 L을 붙혀 이것은 정수형이다 라는것을 알려주어야 한다.
+         *
+         * */
+
+        long var1 = 10;
+        long var2 = 20L;
+
+        long var3 = 10000000000L;
+
+    }
+}
