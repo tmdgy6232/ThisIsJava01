ee00c700b6ae7bf15e4d4465536b98d7720eef86
tmdgy6232 <tmdgy6232@naver.com>
2020-08-17 20:37:48 +0900

    method overide

---
+++ src/sec06/exam13_override/pakege1/A.java
@@ -0,0 +1,11 @@
+package sec06.exam13_override.pakege1;
+
+public class A {
+    protected  String field;
+
+    protected  A(){};
+
+    protected void method(){
+
+    }
+}
---
+++ src/sec06/exam13_override/pakege2/C.java
@@ -0,0 +1,13 @@
+package sec06.exam13_override.pakege2;
+
+import sec06.exam13_override.pakege1.A;
+
+public class C {
+
+    public void method(){
+        //A a  = new A();
+        //a.field = "value";
+        // a.method();
+
+    }
+}
