0758bd4b886818b460b53c5201f7e33d337d105a
tmdgy6232 <tmdgy6232@naver.com>
2020-08-18 22:45:04 +0900

    상속

---
+++ src/sec06/exam14_Promotion/PromotionExample.java
@@ -0,0 +1,19 @@
+package sec06.exam14_Promotion;
+
+public class PromotionExample {
+    class A{}
+    class B extends A{};
+    class C extends A{};
+
+    class D extends B{};
+    class E extends C{};
+
+    public static void main(String[] args) {
+        B b = new B();
+        C c = new C();
+        D d = new D();
+
+        A A1 = b;
+        
+    }
+}
