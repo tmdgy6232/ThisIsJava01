d64aee95c11fb073a3b74ebca5070f9eac529a2c
tmdgy6232 <tmdgy6232@naver.com>
2020-08-19 23:39:18 +0900

    매개변수의 다형성

--- src/sec06/exam15_polymopism/PolymopismExample.java
+++ src/sec06/exam15_polymopism/PolymopismExample.java
@@ -8,4 +8,11 @@ public class PolymopismExample {
     public void method(){
         System.out.println("aptjem zzz");;
     }
+
+    /**
+     *  ㅂ클래스의 다형성 뿐만 아니라 매개변수도 부모 클래스를 사용했으면
+     *  매개변수로 자식클래스도 사용할 수 있다.
+     *  이걸 매개변수의 다형성이라 한다.
+     *
+     * */
 }
