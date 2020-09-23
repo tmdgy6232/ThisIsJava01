a17e43d15862e94c1087a9806c87386174c0d063
tmdgy6232 <tmdgy6232@naver.com>
2020-09-23 23:44:58 +0900

    interface 상수

---
+++ src/sec08/exam_01_interface/RemoteControl.java
@@ -0,0 +1,11 @@
+package sec08.exam_01_interface;
+
+public interface RemoteControl {
+    public int MAX_VOLUME = 10;
+    public int MIN_VOLUME = 1;
+    /**
+     * 인터페이스의 상수필드는 대문자로 선언한다.
+     * 단어의 조합일 경우 _로 구분한다.
+     *
+     * */
+}
