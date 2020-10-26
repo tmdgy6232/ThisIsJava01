7c7d76535a12a1d3e881cc9aceb66208e9b6e956
tmdgy6232 <tmdgy6232@naver.com>
2020-10-26 23:51:08 +0900

    정규식

---
+++ src/sec11/exam_01_Api/String/PatternExample.java
@@ -0,0 +1,26 @@
+package sec11.exam_01_Api.String;
+
+import java.util.regex.Pattern;
+
+public class PatternExample {
+    public static void main(String[] args) {
+        String regExp = "(02|010)-\\d{3,4}-\\d{4}";
+        String data = "010-123-4567";
+        boolean result = Pattern.matches(regExp, data);
+        if (result){
+            System.out.println("정규식과 일치합니다.");
+        } else {
+            System.out.println("정규식과 일치하지 않습니다.");
+        }
+
+        regExp = "\\w+@\\w+\\. \\w+(\\. \\w+)?^";
+        data = "ang1e12l@naver.com12";
+        result = Pattern.matches(regExp, data);
+        if (result){
+            System.out.println("정규식과 일치합니다.");
+        } else {
+            System.out.println("정규식과 일치하지 않습니다.");
+        }
+
+    }
+}
