c8231b32b393aa00029744999b80feeb6d76de59
tmdgy6232 <tmdgy6232@naver.com>
2020-12-01 23:58:23 +0900

    람다식으로 익명구현객체 생성

--- src/sec14/exam_01_lamda/LamdaExample.java
+++ src/sec14/exam_01_lamda/LamdaExample.java
@@ -2,6 +2,13 @@ package sec14.exam_01_lamda;
 
 public class LamdaExample {
     public static void main(String[] args) {
-        
+
+        Runnable runnable = () -> {
+          System.out.println("시작");
+        };
+
+        Thread thread = new Thread(runnable);
+
+        thread.start();
     }
 }
