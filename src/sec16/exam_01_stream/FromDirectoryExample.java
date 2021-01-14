f7c6ae25094278336f720c70c3b1c5cfb2f114da
tmdgy6232 <tmdgy6232@naver.com>
2021-01-14 22:20:53 +0900

    directory get stream

---
+++ src/sec16/exam_01_stream/FromDirectoryExample.java
@@ -0,0 +1,17 @@
+package sec16.exam_01_stream;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FromDirectoryExample {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src");
+        Stream<Path> stream = Files.list(path);
+        stream.forEach( p -> System.out.println(p.getFileName()));
+    }
+}
