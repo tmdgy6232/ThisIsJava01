6a981ef4cc9b814b53ad0abe5557040f81c2bfd9
tmdgy6232 <tmdgy6232@naver.com>
2021-03-11 23:47:01 +0900

    FileSystem Class

---
+++ src/sec19/exam_01_network/FileSystemExample.java
@@ -0,0 +1,25 @@
+package sec19.exam_01_network;
+
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+
+public class FileSystemExample {
+    public static void main(String[] args) throws IOException {
+        FileSystem fileSystem = FileSystems.getDefault();
+
+        for (FileStore fileStore : fileSystem.getFileStores()){
+            System.out.println("드라이버 명 : " + fileStore.name());
+            System.out.println("파일시스템 : " + fileStore.type());
+            System.out.println("전체공간 : " + fileStore.getTotalSpace() + "byte");
+            System.out.println("사용중인 공간  : " + (fileStore.getTotalSpace() - fileStore.getUnallocatedSpace()) + "byte");
+            System.out.println("사용 가능한 공간 : " + (fileStore.getUsableSpace()) + "byte");
+            System.out.println();
+        }
+        System.out.println("파일 구분자 : " +fileSystem.getSeparator());
+        System.out.println();
+
+
+    }
+}
